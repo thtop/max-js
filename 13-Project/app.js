803dc2c4ace976080e3ba1c86d137e9464022462
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-27 14:31:04 +0700

    Http Request Setup

--- 13-Project/app.js
+++ 13-Project/app.js
@@ -1,7 +1,29 @@
 "use strict";
 
+
+// da34e0c1df77595eba79288f038ae34e
+
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    console.log(searchCity.value);
+    var cityName = searchCity.value;
+
+    //console.log(cityName);
+
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name.');
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = 'da34e0c1df77595eba79288f038ae34e';
+    var url = `api.openweathermap.org/data/2.5/weather?q=${cityName}`;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+
+        } else if (http.readyState == XMLHttpRequest.DONE && http.status !== 200) {
+            alert('Something went wrong!');
+        }
+    } 
 }
