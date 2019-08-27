d0428b6a025df51ef882af53878e3773dd38335b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-27 13:19:42 +0700

    Changing Propagation Order

--- 10-Events/05-PropagationOrder.js
+++ 10-Events/05-PropagationOrder.js
@@ -2,7 +2,7 @@ var inner = document.querySelector('#inner');
 var outer = document.querySelector('#outer');
 
 inner.addEventListener('click', innerListener);
-outer.addEventListener('click', outerListener, true);
+outer.addEventListener('click', outerListener, true); // add true
 
 function innerListener(event) {
     //event.stopPropagation();
