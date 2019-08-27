e3273d9a5b60cbcebbb80975a5fcceb29436afd9
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-27 13:18:24 +0700

    Module Resources

---
+++ 10-Events/05-PropagationOrder.js
@@ -0,0 +1,14 @@
+var inner = document.querySelector('#inner');
+var outer = document.querySelector('#outer');
+
+inner.addEventListener('click', innerListener);
+outer.addEventListener('click', outerListener, true);
+
+function innerListener(event) {
+    //event.stopPropagation();
+    console.log('Clicked inner');
+}
+
+function outerListener(event) {
+    console.log('Clicked outer');
+}
