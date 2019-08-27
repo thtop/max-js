f50651b9626f595d069639ff4989d554e1b928ac
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-27 11:51:21 +0700

    Event Handlers

---
+++ 10-Events/01-EventHandlers.js
@@ -0,0 +1,19 @@
+// window.onload = function () {
+//     console.log('Window loaded!');
+// }
+
+var btn = document.querySelector('button');
+
+// document.onclick = function () {
+//     console.log('Clicked!');
+// }
+
+btn.onclick = function () {
+    console.log('Clicked!');
+}
+
+btn.onclick = function () {
+    console.log('Also clicked!');
+}
+
+
